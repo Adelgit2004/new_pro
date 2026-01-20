b55063c8458842aa3ac97af69b689b76f7bb7a43
Adelgit2004 <adeljobygit@gmail.com>
2026-01-20 18:59:24 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,12 @@
+import TextToSpeech from "./SpeechToText";
 
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>Text to Speech</h2>
+      <TextToSpeech />
+    </div>
+  );
+}
+
+export default App;
