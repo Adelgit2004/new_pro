5e6376d530f942677a4bac2c8dc5cd8bbad8bd05
Adelgit2004 <adeljobygit@gmail.com>
2026-01-20 21:57:06 +0530

    Update LanguageSelector.js

--- frontend/src/components/LanguageSelector.js
+++ frontend/src/components/LanguageSelector.js
@@ -1,15 +1,11 @@
-const voices = ["English", "Hindi", "Spanish","Malayalam"];
+const voices = ["English", "Hindi", "Spanish", "Malayalam"];
 
 export default function LanguageSelector({ language, setLanguage }) {
   return (
-    <select
-      value={language}
-      onChange={(e) => setLanguage(e.target.value)}
-    >
-      {voices.map(lang => (
+    <select value={language} onChange={(e) => setLanguage(e.target.value)}>
+      {voices.map((lang) => (
         <option key={lang}>{lang}</option>
       ))}
     </select>
   );
 }
-
